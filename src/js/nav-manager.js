9f1cd1f647206f771901837d09a8c68d124a2890
Matt Keenan <mattkeenan@Matts-MacBook-Pro.local>
2018-05-24 22:27:33 -0400

    Mobile nav integration

---
+++ src/js/nav-manager.js
@@ -0,0 +1,32 @@
+var screenHeight, screenWidth;
+screenHeight = screen.height;
+screenWidth = screen.width;
+
+if (screenWidth <= 575) {
+    console.log('Hit');
+
+    document.getElementById('nav').style.transform = ('translateY(-200px)');
+}
+
+window.addEventListener('resize', () => {
+    screenWidth = screen.width;
+    setScreenSize(screenWidth);
+});
+
+function setScreenSize(width) {
+    if (width <= 575) {
+        document.getElementById('nav').style.transform = ('translateY(-200px)');
+    } else {
+        document.getElementById('nav').style.transform = ('translateY(0px)');
+    }
+}
+
+function showMenu(isShowing) {
+    screenWidth = screen.width;
+    if (screenWidth > 575) { return; }
+    if (isShowing === true) {
+        document.getElementById('nav').style.transform = ('translateY(0px)');
+    } else {
+        document.getElementById('nav').style.transform = ('translateY(-200px)');
+    }
+}
